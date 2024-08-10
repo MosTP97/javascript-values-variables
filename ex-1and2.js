c070a0aa6ed83e615b7c770eaa272aac99828be7
Mos <theeraphat.sukn@gmail.com>
2024-08-11 00:07:47 +0700

    edit JavaScript Values Variables Assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "karter";
+let lastName = "Abderson";
+let Address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const bill = 15000;
+const restaurantCountOpen = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+Address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+bill = 5000;
+restaurantCountOpen = 2500;
